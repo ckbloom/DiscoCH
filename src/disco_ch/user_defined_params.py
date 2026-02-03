6dbf33e12965531e4dbf6e6dc264dede3183e370
bloomcol <colin.bloom@wsl.ch>
2026-02-03 13:44:09 +0100

    Updates for memory management and cloud computing

--- src/disco_ch/user_defined_params.py
+++ src/disco_ch/user_defined_params.py
@@ -9,13 +9,13 @@ stac_location = "https://data.geo.admin.ch/api/stac/v0.9/"
 existing_data = r"B:/minmax_demo"
 
 # Forest mask location - this should point to the downloaded forest mask.
-forest_mask = r"G:\1_cbloom\Projects\2025_01_12_VHI\DiscoCH\data\DRAINS_Forest_Mask.tif"
+forest_mask = r"DiscoCH/data/CH_NoValue_255.tif"
 
 # Template raster with NaN extent (same resolution and CRS) - this should point to the downloaded template.
-ch_template = r"G:\1_cbloom\Projects\2025_01_12_VHI\DiscoCH\data\CH_NoValue_255.tif"
+ch_template = r"DiscoCH/data/CH_NoValue_255.tif"
 
 # Trained discoloration model - this should point to the downloaded model pickle file.
-disco_model = r"G:\1_cbloom\Projects\2025_01_12_VHI\DiscoCH\data\empirical_discoloration_model_pipeline_2025_6_2.pkl"
+disco_model = r"DiscoCH/data/empirical_discoloration_model_pipeline_2025_6_2.pkl"
 
 # Output directory - an empty folder where you want to save model outputs (.tif and .png files for each valid date)
 output_dir = r"G:\1_cbloom\Projects\2025_01_12_VHI\DiscoCH\output\demo"
